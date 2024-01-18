e402a39e401a04f1d5b72b899dbad858dd3928be
shankem2 <malavika.shanker2@mail.dcu.ie>
2024-01-18 15:20:59 +0000

    3 letters

---
+++ app/src/main/java/com/yp/tracenlearn/IActivity.java
@@ -0,0 +1,17 @@
+package com.yp.tracenlearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_i);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/yp/tracenlearn/T_Activity.java
@@ -0,0 +1,14 @@
+package com.yp.tracenlearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class T_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t);
+    }
+}
