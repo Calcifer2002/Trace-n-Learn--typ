1a97ab8cff6eeabe1a9333b8b4e69da867d261e7
shankem2 <malavika.shanker2@mail.dcu.ie>
2024-02-04 21:06:45 +0000

    finished b

--- app/src/main/java/com/yp/tracenlearn/MainActivity.java
+++ app/src/main/java/com/yp/tracenlearn/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
 
                                         if (dataSnapshot.child("username").exists()) {
 
-                                            Intent i = new Intent(MainActivity.this, T_Activity.class);
+                                            Intent i = new Intent(MainActivity.this, E_Activity.class);
                                             startActivity(i);
                                             finish();
                                         } else {
